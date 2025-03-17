4247e3c12846036779b7f6c4bcfc4d2fe15276bb
harshith8899 <harshithsham8899@gmail.com>
2025-03-17 21:16:23 +0530

    functionality updated

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './TicTacToe.css';
 import circle_icon from '../assets/circle.png';
 import cross_icon from '../assets/cross.png';
@@ -9,10 +9,11 @@ const TicTacToe = () => { // Changed function name to PascalCase
 
   let [count, setCount] = useState(0); // Fixed 'UseState' to 'useState'
   let [lock, setLock] = useState(false);
+  let titleRef = useRef(null);
 
   const toggle = (e, num) => { // Added missing 'e' and 'num' parameters
     if (lock) {
-      return;
+      return;   
     }
     if (count % 2 === 0) {
       e.target.innerHTML = `<img src="${cross_icon}" alt="X">`; // Fixed incorrect string interpolation
@@ -23,12 +24,68 @@ const TicTacToe = () => { // Changed function name to PascalCase
       data[num] = "o";
       setCount(count + 1);
     }
+    checkWin();
   };
+
+  const checkWin = () => {
+    {/*Checking for winner horizonatlly */}
+    if(data[0]===data[1] && data[1]===data[2] && data[2]!=="")
+    {
+      won(data[2]);
+    }
+    else if(data[3]===data[4] && data[4]===data[5] && data[5]!=="")
+    {
+      won(data[5]);
+    }
+    else if(data[6]===data[7] && data[7]===data[8] && data[8]!=="")
+    {
+      won(data[8]);
+    }
+
+    // Checking for winner vertically
+
+    else if(data[0]===data[3] && data[3]===data[6] && data[6]!=="")
+    {
+      won(data[6]);
+    }
+    else if(data[1]===data[4] && data[4]===data[7] && data[7]!=="")
+      {
+        won(data[7]);
+      }
+    else if(data[2]===data[5] && data[5]===data[8] && data[8]!=="")
+        {
+          won(data[8]);
+        }
+
+     // checking for winner diagonally
+
+     else if(data[0]===data[4] && data[4]===data[8] && data[8]!=="")
+      {
+        won(data[8]);
+      }
+      else if(data[2]===data[4] && data[4]===data[6] && data[6]!=="")
+        {
+          won(data[6]);
+        }
+  }
+
+  const won = (winner) => {
+    setLock(true);
+    if (winner==='x')
+    {
+      titleRef.current.innerHTML = `Congratulation: <img src=${cross_icon}>Won`;
+    }
+    else
+    {
+      titleRef.current.innerHTML = `Congratulation: <img src=${circle_icon}>Won`;
+
+    }
+  }
 
   return (
     <div>
       <div className="container">
-        <h1 className="title">Tic Tac Toe in <span>React</span></h1>
+        <h1 className="title" ref={titleRef}>Tic Tac Toe in <span>React</span></h1>
         <div className="board">
           <div className="row1"> 
             <div className="boxes" onClick={(e) => toggle(e, 0)}></div>
